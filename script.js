6a184565703572a541a7cdcbe19234a271cbdec4
GiorgioDordo <djole6789@gmail.com>
2024-08-29 19:27:08 +0200

    levels

--- script.js
+++ script.js
@@ -2,48 +2,49 @@
 const grid = document.getElementById('grid');
 const play = document.getElementById('play-btn');
 const form = document.querySelector('form');
-
-
+const levels = document.getElementById('levels');
 // const rows = 10;
 // const cols = 10;
 // const totalCells = rows * cols;
 
 // function to create cells 
-const createCell = (content = '') => {
+const createCell = (content= '', level) => {
     const cell = document.createElement('div');
-    cell.className = 'cell';
+    cell.classList.add('cell', level)
     cell.append(content);
+
     return cell;
 }
 
 // create cells
 form.addEventListener('submit', function(e){
     e.preventDefault();
-
-    const levels = document.getElementById('levels');
     
-    const level = parseInt(levelsSelect.value);
+    grid.innerHTML = '';
+
+    const level = levels.value;
     let rows;
     let cols;
-
+    
     switch(level) {
         case 'hard':
-            rows = 10;
-            cols = 10;
+            rows = 7;
+            cols = 7;
             break;
         case 'normal':
             rows = 9;
             cols = 9;
             break;
         case 'easy':
-            rows = 7;
-            cols = 7;
+            rows = 10;
+            cols = 10;
             break;        
     }
-    const tatalCells = rows * cols;
 
-    for (let i = 0; i < totalCells; i++) {
-        const cell = createCell(i + 1);
+    const totalCells = rows * cols;
+
+    for (let i = 1; i <= totalCells; i++) {
+        const cell = createCell(i, level);
 
         // print in console and color the cells when clicked
         cell.addEventListener('click', () => {
