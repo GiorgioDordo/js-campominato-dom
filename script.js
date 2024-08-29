79cf575caca9a0c14f6ded047ff2b0e34625fbde
GiorgioDordo <djole6789@gmail.com>
2024-08-29 17:59:19 +0200

    levels

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // get elements from dom
 const grid = document.getElementById('grid');
 const play = document.getElementById('play-btn');
+const form = document.querySelector('form');
 
-const rows = 10;
-const cols = 10;
-const totalCells = rows * cols;
+
+// const rows = 10;
+// const cols = 10;
+// const totalCells = rows * cols;
 
 // function to create cells 
 const createCell = (content = '') => {
@@ -15,8 +17,30 @@ const createCell = (content = '') => {
 }
 
 // create cells
-play.addEventListener('click', function(e){
+form.addEventListener('submit', function(e){
     e.preventDefault();
+
+    const levels = document.getElementById('levels');
+    
+    const level = parseInt(levelsSelect.value);
+    let rows;
+    let cols;
+
+    switch(level) {
+        case 'hard':
+            rows = 10;
+            cols = 10;
+            break;
+        case 'normal':
+            rows = 9;
+            cols = 9;
+            break;
+        case 'easy':
+            rows = 7;
+            cols = 7;
+            break;        
+    }
+    const tatalCells = rows * cols;
 
     for (let i = 0; i < totalCells; i++) {
         const cell = createCell(i + 1);
